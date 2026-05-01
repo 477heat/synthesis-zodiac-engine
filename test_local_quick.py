c13572cd52b183a5840a9d2022c605a021da0d81
477heat <477heat@gmail.com>
2026-05-01 16:07:12 -0600

    Updated Elemental Synergy

--- test_local_quick.py
+++ test_local_quick.py
@@ -4,7 +4,7 @@ from lambda_function import lambda_handler
 # Simulate an API Gateway event
 event = {
     "body": json.dumps({
-        "dob": "1992-05-20",
+        "dob": "1980-02-16",
         "currentDate": "2026-05-01"
     })
 }
